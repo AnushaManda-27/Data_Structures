c6a330c79b76a60f2e21fd2045dab244164d4a14
AnushaManda-27 <anushamanda27@gmail.com>
2021-08-10 06:51:59 +0530

    [Anusha] Add: iterate through items in dictionary

--- Data_Strucure_practice/Dictionaries/for_loop.py
+++ Data_Strucure_practice/Dictionaries/for_loop.py
@@ -7,7 +7,7 @@
 @Title: iterating key, values of a dictionary through for loop
 '''
 
-dict = {'fruit': 'apple', 'veg': 'beets', 'greens': 'spinach'}
-for key,value in dict.items():
+dictionary = {'fruit': 'apple', 'veg': 'beets', 'greens': 'spinach'}
+for key,value in dictionary.items():
     print(key,value)
     
